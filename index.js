3a9eef5ee4c78b08768d51cb62f834bcb022e31f
nairx <nairxprv@gmail.com>
2025-06-03 15:39:09 +0530

    Updated

--- index.js
+++ index.js
@@ -4,14 +4,15 @@ import cors from "cors";
 
 import userRouter from "./routes/userRoutes.js";
 import productRouter from "./routes/productRoutes.js";
+import orderRouter from "./routes/orderRoutes.js";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 app.use("/users", userRouter);
-
 app.use("/products", productRouter);
+app.use("/orders",orderRouter)
 
 app.listen(8080, () => {
   mongoose.connect("mongodb://localhost:27017/gcet");
